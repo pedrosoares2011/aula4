d6b0237610314ccc5b84437a5893bf9ba80af9d9
pedrosoares2011 <morgado.1908@aluno.pr.senac.br>
2026-02-23 16:26:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,14 @@
 let idadeUser = prompt("Digite sua idade:");
 let estadoUser = prompt("Digite seu estado").toUpperCase(); 
 
-if (idadeUser <= 18 && estadoUser === "PR") {
-    document.getElementById("lascou").innerText = "hora de dar tchau!";
-    document.getElementById("Minecraft").innerText = "hora de dar tchau!";
-    document.getElementById("Bem vindo").innerText = "hora de dar tchau!";
-   
-
-
+if (idadeUser >= 18 && estadoUser === "PR") {
+    document.getElementById("lascou").innerText = "Uau";
+    document.getElementById("uau").innerText = "Vc Tem a Idade";
+    document.getElementById("ola").innerText = "Parabens";
+    document.getElementById("eita").src = "https://s2-oglobo.glbimg.com/9ZhBOSojJWmr7r7tYtptNBhWwoo=/0x0:1600x1107/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2023/Z/R/HNX0JTSuaKPpQHSA28Rg/00000175-p.jpg";
 } else {
-       document.getElementById("Olá").innerText = "O My God";
-       document.getElementById("Meu nome é Pedro").innerText = "hora de dar tchau!";
-       document.getElementById("Terra").innerText = "hora de dar tchau!";
-
-      
+       document.getElementById("lascou").innerText = "kkkk";
+       document.getElementById("uau").innerText = "Vc Não Tem Idade";
+       document.getElementById("ola").innerText = "hora de dar tchau";
+       document.getElementById("eita").src = "https://i0.statig.com.br/bancodeimagens/04/gh/20/04gh20m4i4q6ensy5v8n7bfur.jpg"
 }
-   
-    
